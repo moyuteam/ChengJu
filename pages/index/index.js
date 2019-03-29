c48409729def154eb2bd9d303292c6f6a2f1a528
fan5shi <963326056@qq.com>
2019-03-29 17:10:36 +0800

    new index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,80 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/index1/index1.js
+const order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    message: 'Hello Minia~',
+    toView: 'red',
+    scrollTop: 100,
+    imageArray: [
+      {
+        color: 'white',
+        image: '../image/Eat.png',
+        title: 'Eat at dining hall',
+        time: '12:00~13:00  4-1',
+      }, {
+        color: 'yellow',
+        image: '../image/Activity.png',
+        title: 'Flying kites with kids',
+        time: '14:30~17:00  6-1',
+      }, {
+        color: 'blue',
+        image: '../image/Meeting.png',
+        title: 'Meeting about AI',
+        time: '16:00~17:00  5-23~5-25',
+      }, {
+        color: 'green',
+        image: '../image/Sing.png',
+        title: 'Sing aloud in public',
+        time: '20:00~22:00  5-2',
+      }, {
+        color: 'red',
+        image: '../image/Sport.png',
+        title: 'Having sports at night',
+        time: '16:00~17:00  4-6',
+      }, {
+        color: 'white',
+        image: '../image/Study.png',
+        title: 'Self-study with partners',
+        time: '8:00~22:00  3-30',
+      }, {
+        color: 'white',
+        image: '../image/Eat.png',
+        title: 'Seek for the best snack in CD',
+        time: '8:00~12:00  4-12',
+      }, {
+        color: 'white',
+        image: '../image/Meeting.png',
+        title: 'Wellknown people come to speech',
+        time: '14:00~16:00  4-15',
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    ]
+  },
+  upper(e) {
+    console.log(e)
+  },
+  lower(e) {
+    console.log(e)
+  },
+  scroll(e) {
+    console.log(e)
+  },
+  tap(e) {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  tapMove(e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      scrollTop: this.data.scrollTop + 10
     })
   }
 })
