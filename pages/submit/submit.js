844a3edbd420abe6e56e063408e1b55712f2877f
Jenny <261072913@qq.com>
2019-03-30 21:36:35 +0800

    submit

--- pages/submit/submit.js
+++ pages/submit/submit.js
@@ -8,16 +8,7 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    //设置默认的年份
-    this.setData({
-      choose_year: this.data.multiArray[0][0]
-    })
-  },
-  //获取时间日期
+//日期时间选择器
   bindDateChange(e) {
     this.setData({
       date: e.detail.value
@@ -27,5 +18,51 @@ Page({
     this.setData({
       time: e.detail.value
     })
+  },
+
+//上传图片
+uploadImage: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,  //最多可以选择的图片总数
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        //启动上传等待中...
+        wx.showToast({
+          title: '正在上传...',
+          icon: 'loading',
+          mask: true,
+          duration: 10000
+        })
+
+        wx.uploadFile({
+          url: '67.216.202.115',
+          filePath: tempFilePaths[0],
+          name: 'uploadfile_ant',
+          formData: {
+          },
+          header: {
+            "Content-Type": "multipart/form-data"
+          },
+          success: function (res) {
+            var data = JSON.parse(res.data);
+            //服务器返回格式: { "Catalog": "testFolder", "FileName": "1.jpg", "Url": "https://test.com/1.jpg" }
+            console.log(data);
+          },
+          fail: function (res) {
+            wx.hideToast();
+            wx.showModal({
+              title: '错误提示',
+              content: '上传图片失败',
+              showCancel: false,
+              success: function (res) { }
+            })
+          }
+        });
+      }
+    });
   }
 })
