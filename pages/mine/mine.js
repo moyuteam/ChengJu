015ffaf58d5cacb125efca628ccbf11af9af4b37
Jenny <261072913@qq.com>
2019-04-03 16:34:31 +0800

    submit

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    menuTop: ''
   },
 
   /**
@@ -27,6 +27,15 @@ Page({
    */
   onShow: function () {
 
+    var that = this;
+    var query = wx.createSelectorQuery()//创建节点查询器 query
+    query.select('#fix').boundingClientRect()//选择Id的节点，获取节点位置信息的查询请求
+    query.exec(function (res) {
+      console.log(res[0].top); // #fix节点的上边界坐标
+      that.setData({
+        menuTop: res[0].top
+      })
+    });
   },
 
   /**
@@ -62,5 +71,23 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  /**
+   * 监听滚动事件
+   */
+  onPageScroll: function (e) {
+    console.log(e);//{scrollTop:99}
+    var that = this;
+    //当页面滚动距离scrollTop > menuTop某元素距离文档顶部的距离时，某元素固定定位
+    if (e.scrollTop > that.data.menuTop) {
+      that.setData({
+        menuFixed: true
+      })
+    } else {
+      that.setData({
+        menuFixed: false
+      })
+    }
+  }    
 })
