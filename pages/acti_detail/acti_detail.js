1729e141feb8bbe82d61907493c8bc248004db76
fan5shi <963326056@qq.com>
2019-05-30 22:46:21 +0800

    倒数几次提交

--- pages/acti_detail/acti_detail.js
+++ pages/acti_detail/acti_detail.js
@@ -105,7 +105,16 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    if (res.from === 'button') {
 
+    }
+    return {
+      title: '转发',
+      path: '/pages/index/community/topic/topic?jsonStr=' + this.data.list,
+      success: function (res) {
+        console.log('成功', res)
+      }
+    }
   },
 
   onSignIn() {
